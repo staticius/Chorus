54c58b4b9119a6d2c94fcd2c6e8023b0e7287ecb
OmniacDEV <blueleafmonkey.gaming@gmail.com>
2025-04-08 20:53:51 +1200

    errors

--- src/main/kotlin/org/chorus/event/block/BlockSpreadEvent.kt
+++ src/main/kotlin/org/chorus/event/block/BlockSpreadEvent.kt
@@ -4,7 +4,7 @@ import org.chorus.block.Block
 import org.chorus.event.HandlerList
 
 
-class BlockSpreadEvent(block: Block, val source: Block, newState: Block?) :
+class BlockSpreadEvent(block: Block, val source: Block, newState: Block) :
     BlockFormEvent(block, newState) {
     companion object {
         val handlers: HandlerList = HandlerList()
