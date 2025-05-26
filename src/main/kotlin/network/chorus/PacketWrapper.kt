6911933676b5731252cd19749f6e19c7f42b83c5
OmniacDEV <blueleafmonkey.gaming@gmail.com>
2025-05-26 20:38:41 +1200

    protocol

---
+++ src/main/kotlin/network/chorus/PacketWrapper.kt
@@ -0,0 +1,27 @@
+package org.chorus_oss.chorus.network.chorus
+
+import kotlinx.io.Sink
+import kotlinx.io.Source
+import org.chorus_oss.protocol.core.Packet
+import org.chorus_oss.protocol.core.PacketRegistry
+import org.chorus_oss.protocol.core.ProtoCodec
+
+data class PacketWrapper(
+    val header: PacketHeader,
+    val packet: Packet
+) {
+    companion object : ProtoCodec<PacketWrapper> {
+        override fun serialize(value: PacketWrapper, stream: Sink) {
+            PacketHeader.serialize(value.header, stream)
+            PacketRegistry.get(value.packet)?.serialize(value.packet, stream) ?: throw RuntimeException("PacketCodec not found for ${value.packet}")
+        }
+
+        override fun deserialize(stream: Source): PacketWrapper {
+            val header: PacketHeader
+            return PacketWrapper(
+                header = PacketHeader.deserialize(stream).also { header = it },
+                packet = PacketRegistry.get(header.packetID.toInt())?.deserialize(stream) as? Packet ?: throw RuntimeException("PacketCodec not found for id: ${header.packetID}")
+            )
+        }
+    }
+}
