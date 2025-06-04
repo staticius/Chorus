4d0bcbf90d821713227dc7d74a8e213f9a80d3e6
OmniacDEV <blueleafmonkey.gaming@gmail.com>
2025-06-05 00:11:46 +1200

    encryption

--- src/main/kotlin/network/chorus/EncryptionWrapper.kt
+++ src/main/kotlin/network/chorus/EncryptionWrapper.kt
@@ -1,14 +1,16 @@
 package org.chorus_oss.chorus.network.chorus
 
-import dev.whyoleg.cryptography.algorithms.AES
 import kotlinx.io.Buffer
 import kotlinx.io.Sink
 import kotlinx.io.Source
 import org.chorus_oss.protocol.core.ProtoCodec
+import javax.crypto.Cipher
+import javax.crypto.SecretKey
 
 data class EncryptionWrapper(
     val raw: Buffer,
-    val cipher: AES.CTR.Key
+    val key: SecretKey,
+    val cipher: Cipher,
 ) {
     companion object : ProtoCodec<EncryptionWrapper> {
         override fun serialize(value: EncryptionWrapper, stream: Sink) {
