7fc27365275608910d2c0f23bfc4e32941cc728f
OmniacDEV <blueleafmonkey.gaming@gmail.com>
2025-06-14 00:09:56 +1200

    block permutation

--- src/main/kotlin/experimental/block/definitions/Dirt.kt
+++ src/main/kotlin/experimental/block/definitions/Dirt.kt
@@ -1,14 +1,14 @@
 package org.chorus_oss.chorus.experimental.block.definitions
 
 import org.chorus_oss.chorus.experimental.block.BlockDefinition
-import org.chorus_oss.chorus.experimental.block.BlockPermutation
+import org.chorus_oss.chorus.experimental.block.BlockPermutationDefinition
 
 object Dirt : BlockDefinition(
     identifier = "minecraft:dirt",
     states = emptyList(),
     components = emptyList(),
     permutations = listOf(
-        BlockPermutation(
+        BlockPermutationDefinition(
             condition = { it.identifier == "minecraft:dirt" },
             components = emptyList(),
         )
