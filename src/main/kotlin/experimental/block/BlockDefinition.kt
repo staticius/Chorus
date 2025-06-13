042aa50e1464e059417414f976282e260ab780ff
OmniacDEV <blueleafmonkey.gaming@gmail.com>
2025-06-14 00:10:19 +1200

    block definition changes

--- src/main/kotlin/experimental/block/BlockDefinition.kt
+++ src/main/kotlin/experimental/block/BlockDefinition.kt
@@ -1,56 +1,18 @@
 package org.chorus_oss.chorus.experimental.block
 
-import org.chorus_oss.chorus.block.BlockState
-import org.chorus_oss.chorus.block.BlockStateImpl
-import org.chorus_oss.chorus.block.property.type.BlockPropertyType
-import org.chorus_oss.chorus.experimental.utils.BlockStates
+import org.chorus_oss.chorus.experimental.block.state.BlockState
 import org.chorus_oss.chorus.utils.Identifier
 
 abstract class BlockDefinition(
     val identifier: String,
-    val states: List<BlockPropertyType<*>> = emptyList(),
+    val states: List<BlockState<*>> = emptyList(),
     val components: List<BlockComponent> = emptyList(),
-    val permutations: List<BlockPermutation> = emptyList(),
+    val permutations: List<BlockPermutationDefinition> = emptyList(),
 ) {
-    val maskSize: Int = states.sumOf { it.bitSize.toInt() }.also {
-        if (it > 16) throw IllegalStateException("State limit exceeded for block: $identifier")
-    }
-
-    val maskMap: Map<Short, BlockStateImpl> = BlockStates.getMaskMap(identifier, states)
-
-    val defaultState: BlockStateImpl = BlockStates.getDefaultState(identifier, states)
+    val defaultPermutation: BlockPermutation = BlockPermutation.getDefault(identifier, states)
 
     init {
         Identifier.assertValid(identifier)
-    }
-
-    fun getState(mask: Short): BlockStateImpl? {
-        return maskMap[mask]
-    }
-
-//    fun <T> getState(
-//        property: BlockPropertyType<T>, value: T
-//    ): BlockState {
-//        return defaultState.setPropertyValue(this, property, value)
-//    }
-//
-//    fun getState(propertyValue: BlockPropertyValue<*, *, *>): BlockState {
-//        return defaultState.setPropertyValue(this, propertyValue)
-//    }
-//
-//    fun getState(vararg values: BlockPropertyValue<*, *, *>): BlockState {
-//        return defaultState.setPropertyValues(this, *values)
-//    }
-
-    fun hasState(state: BlockState): Boolean {
-        return maskMap.containsValue(state)
-    }
-
-    fun hasState(mask: Short): Boolean {
-        return maskMap.containsKey(mask)
-    }
-
-    fun hasProperty(property: BlockPropertyType<*>): Boolean {
-        return states.contains(property)
+        require(states.sumOf { it.size } <= 16) { "BlockDefinition \"$identifier\" exceeds the state limit" }
     }
 }
