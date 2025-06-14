f01daf74ff44ae702baa168f6830fcbe3b7cf2dd
OmniacDEV <blueleafmonkey.gaming@gmail.com>
2025-06-15 03:48:23 +1200

    flammable component

---
+++ src/main/kotlin/experimental/block/components/FlammableComponent.kt
@@ -0,0 +1,14 @@
+package org.chorus_oss.chorus.experimental.block.components
+
+import com.github.quillraven.fleks.Component
+import com.github.quillraven.fleks.ComponentType
+import org.chorus_oss.chorus.experimental.block.BlockComponent
+
+data class FlammableComponent(
+    val catchChance: Int,
+    val destroyChance: Int,
+): BlockComponent, Component<FlammableComponent> {
+    override fun type(): ComponentType<FlammableComponent> = FlammableComponent
+
+    companion object : ComponentType<FlammableComponent>()
+}
