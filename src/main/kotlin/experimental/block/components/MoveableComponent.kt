96002574679b4bfcd83f8cbd4fca8b15dfc6029f
OmniacDEV <blueleafmonkey.gaming@gmail.com>
2025-06-15 04:07:00 +1200

    moveable component

---
+++ src/main/kotlin/experimental/block/components/MoveableComponent.kt
@@ -0,0 +1,21 @@
+package org.chorus_oss.chorus.experimental.block.components
+
+import com.github.quillraven.fleks.Component
+import com.github.quillraven.fleks.ComponentType
+import org.chorus_oss.chorus.experimental.block.BlockComponent
+
+data class MoveableComponent(
+    val movement: Movement = Movement.Both,
+    val sticky: Boolean = false,
+): BlockComponent, Component<MoveableComponent> {
+    enum class Movement {
+        Both,
+        Pull,
+        Pop,
+        None
+    }
+
+    override fun type(): ComponentType<MoveableComponent> = MoveableComponent
+
+    companion object : ComponentType<MoveableComponent>()
+}
