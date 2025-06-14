633ca3d88a08dbaf6e36a4ee91d12bfff69a9db5
OmniacDEV <blueleafmonkey.gaming@gmail.com>
2025-06-15 03:55:34 +1200

    replaceable component

---
+++ src/main/kotlin/experimental/block/components/ReplaceableComponent.kt
@@ -0,0 +1,7 @@
+package org.chorus_oss.chorus.experimental.block.components
+
+import com.github.quillraven.fleks.EntityTag
+import org.chorus_oss.chorus.experimental.block.BlockComponent
+
+@Suppress("unused")
+data object ReplaceableComponent : BlockComponent, EntityTag()
