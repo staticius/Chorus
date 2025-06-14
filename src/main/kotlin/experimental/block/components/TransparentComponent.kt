9a0a6dc62b32664b31969fe54b6fc6b0acc42fde
OmniacDEV <blueleafmonkey.gaming@gmail.com>
2025-06-15 04:00:51 +1200

    transparent component

---
+++ src/main/kotlin/experimental/block/components/TransparentComponent.kt
@@ -0,0 +1,13 @@
+package org.chorus_oss.chorus.experimental.block.components
+
+import com.github.quillraven.fleks.Component
+import com.github.quillraven.fleks.ComponentType
+import org.chorus_oss.chorus.experimental.block.BlockComponent
+
+data class TransparentComponent(
+    val transparent: Boolean,
+): BlockComponent, Component<TransparentComponent> {
+    override fun type(): ComponentType<TransparentComponent> = TransparentComponent
+
+    companion object : ComponentType<TransparentComponent>()
+}
