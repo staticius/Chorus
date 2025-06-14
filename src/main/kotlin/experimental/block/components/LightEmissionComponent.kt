82aec71119fff6a8775d781ecd01f7f11bcbde31
OmniacDEV <blueleafmonkey.gaming@gmail.com>
2025-06-15 03:51:53 +1200

    light emission component

---
+++ src/main/kotlin/experimental/block/components/LightEmissionComponent.kt
@@ -0,0 +1,13 @@
+package org.chorus_oss.chorus.experimental.block.components
+
+import com.github.quillraven.fleks.Component
+import com.github.quillraven.fleks.ComponentType
+import org.chorus_oss.chorus.experimental.block.BlockComponent
+
+data class LightEmissionComponent(
+    val emission: Int,
+): BlockComponent, Component<LightEmissionComponent> {
+    override fun type(): ComponentType<LightEmissionComponent> = LightEmissionComponent
+
+    companion object : ComponentType<LightEmissionComponent>()
+}
