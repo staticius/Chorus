2bf8a5d6af9bd3e04d65b2dd72f2a7533627d79f
OmniacDEV <blueleafmonkey.gaming@gmail.com>
2025-06-15 03:38:11 +1200

    map color component

---
+++ src/main/kotlin/experimental/block/components/MapColorComponent.kt
@@ -0,0 +1,14 @@
+package org.chorus_oss.chorus.experimental.block.components
+
+import com.github.quillraven.fleks.Component
+import com.github.quillraven.fleks.ComponentType
+import org.chorus_oss.chorus.experimental.block.BlockComponent
+import org.chorus_oss.chorus.utils.BlockColor
+
+data class MapColorComponent(
+    val color: BlockColor,
+) : BlockComponent, Component<MapColorComponent> {
+    override fun type(): ComponentType<MapColorComponent> = MapColorComponent
+
+    companion object : ComponentType<MapColorComponent>()
+}
