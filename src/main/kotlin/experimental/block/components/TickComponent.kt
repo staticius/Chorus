28b59993009f5ae964ce6a7939d45e65abcb705a
OmniacDEV <blueleafmonkey.gaming@gmail.com>
2025-06-15 03:43:23 +1200

    tick component

---
+++ src/main/kotlin/experimental/block/components/TickComponent.kt
@@ -0,0 +1,19 @@
+package org.chorus_oss.chorus.experimental.block.components
+
+import com.github.quillraven.fleks.Component
+import com.github.quillraven.fleks.ComponentType
+import org.chorus_oss.chorus.experimental.block.BlockComponent
+
+@Suppress("unused")
+data class TickComponent(
+    val range: IntRange,
+    val looping: Boolean,
+): BlockComponent, Component<TickComponent> {
+    override fun type(): ComponentType<TickComponent> = TickComponent
+
+    constructor(min: Int, max: Int, looping: Boolean): this(min..max, looping)
+
+    constructor(range: Pair<Int, Int>, looping: Boolean): this(range.first..range.second, looping)
+
+    companion object : ComponentType<TickComponent>()
+}
