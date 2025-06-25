a05979147386020b5c897b47bd28d982a83dfa2b
OmniacDEV <blueleafmonkey.gaming@gmail.com>
2025-06-26 02:43:19 +1200

    fix error

--- src/main/kotlin/experimental/network/protocol/utils/ActorDataMap.kt
+++ src/main/kotlin/experimental/network/protocol/utils/ActorDataMap.kt
@@ -7,7 +7,8 @@ import org.chorus_oss.protocol.types.actor_data.ActorDataMap
 fun ActorDataMap.Companion.from(value: EntityDataMap): ActorDataMap {
     return ActorDataMap().also {
         val transformed = value.entries.map { (key, data) ->
-            ActorDataKey.entries[key.getValue()] to data
+            val actual = ((key.getTransformer() as? java.util.function.Function<Any, *>?)?.apply(data) ?: data)
+            ActorDataKey.entries[key.getValue()] to actual
         }
         it.putAll(transformed)
     }
