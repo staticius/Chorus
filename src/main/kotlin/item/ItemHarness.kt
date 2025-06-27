1de26f180d42fe6a3ced2507441d904df42cccd0
OmniacDEV <blueleafmonkey.gaming@gmail.com>
2025-06-27 18:26:34 +1200

    fix

--- src/main/kotlin/item/ItemHarness.kt
+++ src/main/kotlin/item/ItemHarness.kt
@@ -1,3 +1,5 @@
 package org.chorus_oss.chorus.item
 
-abstract class ItemHarness(id: String) : Item(id)
+abstract class ItemHarness(id: String) : Item(id) {
+    override val maxStackSize: Int = 1
+}
