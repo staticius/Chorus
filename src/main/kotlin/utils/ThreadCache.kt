08974d76df73d27110bc09d821d3436dd06b90ad
OmniacDEV <blueleafmonkey.gaming@gmail.com>
2025-07-05 18:29:09 +1200

    remove IterableThreadLocal

--- src/main/kotlin/utils/ThreadCache.kt
+++ src/main/kotlin/utils/ThreadCache.kt
@@ -3,14 +3,7 @@ package org.chorus_oss.chorus.utils
 import java.io.ByteArrayOutputStream
 
 object ThreadCache {
-    fun clean() {
-        fbaos.clean()
+    val fbaos: ThreadLocal<ByteArrayOutputStream> = ThreadLocal.withInitial {
+        ByteArrayOutputStream(1024)
     }
-
-    val fbaos: IterableThreadLocal<ByteArrayOutputStream> =
-        object : IterableThreadLocal<ByteArrayOutputStream>() {
-            override fun init(): ByteArrayOutputStream {
-                return ByteArrayOutputStream(1024)
-            }
-        }
 }
